463f7ba11ed9343df08a92fc2a29f050c5b8aa67
nancyaggarwal42 <nancyaggarwal149@gmail.com>
2026-05-07 18:29:36 +0530

    fixed mongodb connection

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: ['http://localhost:5173', 'https://ss-final-draft.netlify.app/'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173', 'https://ss-final-draft.netlify.app'], credentials: true }));
 app.use(express.json());
 
 console.log('Mongo db', process.env.MONGO_URI)
